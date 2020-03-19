0b1c4c104369a02a9b6137ac267c45e6337812e8
astyer <adam.styer8@gmail.com>
2020-03-19 15:42:55 -0400

    Added conversion from Android text based xpath to iOS Accessibility IDs. Now also formats final string in JSON for easier pasting into MongoDB.

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ capitalizeText = False
 fixNumbering = False
 count = 1
 
-listFile = open("objsJSON.txt", "r")
+listFile = open("objectsJSON.txt", "r")
 lines = listFile.readlines()
+totalLines = -1
+firstDataLine = -1
+lastDataLine = -1
 finalString = ""
 for line in lines:
+    totalLines += 1
     if '_id' in line or '{' in line or '}' in line:
         continue
+    if firstDataLine == -1:
+        firstDataLine = totalLines
+    else:
+        lastDataLine = totalLines
     if fixNumbering:
         split = line.split('\"')
         finalLine = split[0] + '\"' + str(count) + '\"' + split[2] + '\"' + split[3] + '\"' + split[4]
         count += 1
-    if turnToText:
+    else:
         if line[-2] != ',':
             obj = line.split(':')[1][1:-1]
             if turnToText:
@@ -23,6 +31,10 @@ for line in lines:
                     finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1].upper() + '\']\"'
                 else:
                     finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1] + '\']\"'
+            elif turnTextToAccessIds:
+                text = obj.split('=')[1].strip()[1:-3]
+                realObj = line.split(':')[0]
+                finalLine = realObj + ': \"~' + text + '\"\n'
             else:
                 finalLine = obj + ' : \"\"'
         else:
@@ -32,26 +44,24 @@ for line in lines:
                     finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1].upper() + '\']\",\n'
                 else:
                     finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1] + '\']\",\n'
-            else:
-                finalLine = obj + ' : \"\",\n'
-    elif turnTextToAccessIds:
-        if line[-2] != ',':
-            text = line.split('=')[1][2:-4]
-            if turnToText:
-                finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1] + '\']\"'
-            else:
-                finalLine = obj + ' : \"\"'
-        else:
-            obj = line.split(':')[1][1:-2]
-            if turnToText:
-                if capitalizeText:
-                    finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1].upper() + '\']\",\n'
-                else:
-                    finalLine = obj + ' : \"//android.view.View[@text = \'' + obj[1:-1] + '\']\",\n'
+            elif turnTextToAccessIds:
+                text = obj.split('=')[1].strip()[1:-3]
+                realObj = line.split(':')[0]
+                finalLine = realObj + ': \"~' + text + '\",\n'
             else:
                 finalLine = obj + ' : \"\",\n'
 
     finalString += finalLine
+
+lineCount = 0
+appendBefore = ""
+for line in lines:
+    if lineCount < firstDataLine:
+        appendBefore += line
+    elif lastDataLine < lineCount < len(lines):
+        finalString += line
+    lineCount += 1
+finalString = appendBefore + finalString
 
 print(finalString)
 outputFile = open("output.txt", "w")
